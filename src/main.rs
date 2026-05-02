c0575b1abec3e54b4c2a057ca38abf8d55c00046
Mazako <michal.maziarz12@gmail.com>
2026-05-02 21:33:51 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,9 @@ fn main() {
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
+    let mut command = String::new();
+    io::stdin().read_line(&mut command).unwrap();
+    command = command.trim().to_string();
+    print!("{}: command not found", command);
+    io::stdout().flush().unwrap();
 }
